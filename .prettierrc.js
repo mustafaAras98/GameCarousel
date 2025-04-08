4f28475a4f1695464ad499b5afde6ce0e86cbd25
mustafaAras98 <mustafa_aras_25@hotmail.com>
2025-04-08 14:17:22 +0300

    Prettier configure file added to gitignore
    Prettier configure file added to gitignore

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
+  arrowParens: 'always',
   bracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
 };
