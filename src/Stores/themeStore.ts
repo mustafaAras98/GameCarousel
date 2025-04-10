fa0430346a7388d77d5dd0d9bd859a1180abbaf4
mustafaAras98 <mustafa_aras_25@hotmail.com>
2025-04-10 16:56:42 +0300

    ThemeStore updated for initializeTheme and tests are written
    ThemeStore updated for initializeTheme and tests are written for it

--- src/Stores/themeStore.ts
+++ src/Stores/themeStore.ts
@@ -3,28 +3,40 @@ import {create} from 'zustand';
 import {createJSONStorage, devtools, persist} from 'zustand/middleware';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export enum Theme {
+export enum ThemeType {
   LightTheme = 'LightTheme',
   DarkTheme = 'DarkTheme',
 }
 
-type ThemeMode = Theme.DarkTheme | Theme.LightTheme;
+type ThemeMode = ThemeType.DarkTheme | ThemeType.LightTheme;
 
 export interface ThemeState {
   theme: ThemeMode;
   setTheme: (theme: ThemeMode) => void;
+  initializeTheme: () => Promise<void>;
 }
-const getInitialTheme = (): ThemeMode => {
-  const systemScheme = Appearance.getColorScheme();
-  return systemScheme === 'dark' ? Theme.DarkTheme : Theme.LightTheme;
-};
 
 export const useThemeStore = create<ThemeState>()(
   devtools(
     persist(
       (set) => ({
-        theme: getInitialTheme(),
+        theme: ThemeType.LightTheme,
         setTheme: (theme: ThemeMode) => set({theme}),
+        initializeTheme: async () => {
+          try {
+            const savedTheme = await AsyncStorage.getItem('themeStore');
+            if (!savedTheme) {
+              const colorScheme = Appearance.getColorScheme();
+              const schemeTheme: ThemeType =
+                colorScheme === 'dark'
+                  ? ThemeType.DarkTheme
+                  : ThemeType.LightTheme;
+              useThemeStore.getState().setTheme(schemeTheme);
+            }
+          } catch (error) {
+            console.error('Error loading theme from storage:', error);
+          }
+        },
       }),
       {name: 'themeStore', storage: createJSONStorage(() => AsyncStorage)}
     )
